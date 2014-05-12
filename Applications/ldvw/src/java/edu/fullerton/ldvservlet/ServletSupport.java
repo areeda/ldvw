b62c14a74a8626ed2dd19b113e4ba7ae5678f758
Joseph Areeda <joseph.areeda@ligo.org>
2014-05-12 11:40:51 +0000

    v 2.0.7 - Add page header and navbar to support module for consistency among servlets

--- Applications/ldvw/src/java/edu/fullerton/ldvservlet/ServletSupport.java
+++ Applications/ldvw/src/java/edu/fullerton/ldvservlet/ServletSupport.java
@@ -20,6 +20,15 @@ package edu.fullerton.ldvservlet;
 import com.areeda.jaDatabaseSupport.Database;
 import com.areeda.jaDatabaseSupport.Table;
 import edu.fullerton.jspWebUtils.Page;
+import edu.fullerton.jspWebUtils.PageItem;
+import edu.fullerton.jspWebUtils.PageItemBSNavBar;
+import edu.fullerton.jspWebUtils.PageItemImage;
+import edu.fullerton.jspWebUtils.PageItemList;
+import edu.fullerton.jspWebUtils.PageItemString;
+import edu.fullerton.jspWebUtils.PageItemTextLink;
+import edu.fullerton.jspWebUtils.PageTable;
+import edu.fullerton.jspWebUtils.PageTableColumn;
+import edu.fullerton.jspWebUtils.PageTableRow;
 import edu.fullerton.jspWebUtils.WebUtilException;
 import edu.fullerton.ldvjutils.LdvTableException;
 import edu.fullerton.ldvtables.UseLog;
@@ -29,8 +38,6 @@ import java.io.PrintWriter;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -207,7 +214,185 @@ public class ServletSupport
         vpage.includeCSS(jqueryCss);
     }
     
-    
+    public void addStandardHeader(String version) throws WebUtilException
+    {
+        PageTable hdrTbl = new PageTable();
+
+        hdrTbl.setClassName("hdrTable");
+        PageTableRow hdrRow = new PageTableRow();
+        PageItemImage h1 = new PageItemImage(contextPath + "/LIGO_logo50a.png", "LigoDV-Web", "LigoDV-Web");
+
+        h1.setDim(70, 50);
+        PageTableColumn h1Col = new PageTableColumn(h1);
+        h1Col.setId("hdrImg");
+        hdrRow.add(h1Col);
+
+        PageItemList l2 = new PageItemList();
+        String progId;
+        if (version.isEmpty())
+        {
+            progId = "LigoDV-Web <br>";
+        }
+        else
+        {
+            progId = String.format("LigoDV-Web &mdash; <i>v%1$s</i><br>",version);
+        }
+        PageItemString h2 = new PageItemString(progId, false);
+        h2.setClassName("title");
+        l2.add(h2);
+
+        String name = vuser.getCn();
+        if (vuser.isAdmin())
+        {
+            name += " (admin)";
+        }
+        else if (vuser.isTester())
+        {
+            name += " (Experimental commands enabled)";
+        }
+        PageItemString u = new PageItemString("Welcome " + name, false);
+        l2.add(u);
+
+        PageTableColumn h2Col = new PageTableColumn(l2);
+        h2Col.setId("hdrTxt");
+        hdrRow.add(h2Col);
+
+        hdrTbl.addRow(hdrRow);
+        vpage.add(hdrTbl);
+
+        if (isPrototype && isNewSession)
+        {
+            PageItemList protoWarnMsg = new PageItemList();
+            protoWarnMsg.setId("warnDiv");
+            protoWarnMsg.addEvent("onclick", "jQuery('#warnDiv').hide();");
+
+            PageItemString warn = new PageItemString("WARNING");
+            warn.setId("warnTitle");
+            protoWarnMsg.add(warn);
+            protoWarnMsg.addLine("Click in the pannel to hide this warning message.<br>");
+
+            String warnMsg = "This LigoDV-Web server is the prototyping system used to test new and "
+                             + "experimental software.  You are more than welcome to use the site but be forewarned "
+                             + "that the level of bugs and other strange behavior is likely to be higher here "
+                             + "than on the production server.  Also this server is subject to unannounced "
+                             + "brief outages as we load the next version.  Here's a link to the production server ";
+            protoWarnMsg.add(warnMsg);
+            PageItemTextLink realServer = new PageItemTextLink("https://ldvw.ligo.caltech.edu/ldvw/view", "ldvw.ligo.caltech.edu");
+            protoWarnMsg.add(realServer);
+
+            vpage.add(protoWarnMsg);
+        }
+
+    }
+    /**
+     * Add a table of links to our main pages.
+     *
+     * @param imgCnt total number of records in selection
+     */
+    public void addNavBar() throws WebUtilException
+    {
+        String mainHelpUrl = getHelpUrl();
+        // Top row commands 2 element rows "Text", "get parameters" or "http*://"
+        String[][] commands =
+        {
+            {
+                "Home", "main"
+            },
+            {
+                "Saved Plots", "ImageHistory&amp;size=med"
+            },
+            {
+                "Chan Stats", "ChannelStats"
+            },
+            {
+                "NDS Status", "ndsStatus"
+            },
+            {
+                "Upload", "upload"
+            },
+            {
+                "Help", mainHelpUrl
+            },
+            {
+                "Contact Us", "contactUs"
+            }
+        };
+        // Commands only available to admin group
+        String[][] adminCommands =
+        {
+            {
+                "User Stats", "Stats"
+            },
+            {
+                "Edit Help", "EditHelp"
+            },
+            {
+                "DB stats", "dbstats"
+            },
+            
+            {
+                "Servers", "serverManager"
+            }
+        };
+        // commands available to admins or testers
+        String[][] experimentalCommands =
+        {
+            {
+                "New Chan sel", "baseChan"
+            }
+        };
+
+        PageItem navBar;
+
+        PageItemBSNavBar bsnav = new PageItemBSNavBar();
+        String baseUrl = contextPath + "/view?act=";
+        String cmdUrl;
+
+        for (String[] command : commands)
+        {
+            if (!command[0].isEmpty() && !command[1].isEmpty())
+            {
+                if (command[1].matches("^http.?://.*"))
+                {
+                    bsnav.addLink(command[1], command[0], "_blank");
+                }
+                else
+                {
+                    cmdUrl = baseUrl + command[1];
+                    bsnav.addLink(cmdUrl, command[0]);
+                }
+            }
+        }
+
+        if (vuser.isTester())
+        {
+            bsnav.createNewSubmenu("Experimental");
+
+            for (String[] command : experimentalCommands)
+            {
+                cmdUrl = baseUrl + command[1];
+                bsnav.addSubmenuLink(cmdUrl, command[0]);
+            }
+            bsnav.addCurSubmenu();
+        }
+        if (vuser.isAdmin())
+        {
+            bsnav.createNewSubmenu("Admin");
+
+            for (String[] command : adminCommands)
+            {
+                cmdUrl = baseUrl + command[1];
+                bsnav.addSubmenuLink(cmdUrl, command[0]);
+            }
+            bsnav.addCurSubmenu();
+        }
+
+        navBar = bsnav;
+
+        vpage.add(navBar);
+        vpage.addBlankLines(2);
+    }
+
     //=============================GETTERS and SETTERS================
     public String getHelpUrl()
     {
@@ -258,6 +443,15 @@ public class ServletSupport
     public UseLog getuLog()
     {
         return uLog;
+    }
+
+    void close()
+    {
+        if (db!= null)
+        {
+            db.close();
+        }
+        db = null;
     }
  
     
