edd6744e4b6cff9fc2feab15a7cbd2e1036310c9
Joseph Areeda <joseph.areeda@ligo.org>
2014-05-12 11:47:57 +0000

    v 2.0.7 - add  show by Id to be able to hide individual tables in source data list

--- Applications/ldvw/web/jquery/showByClass.js
+++ Applications/ldvw/web/jquery/showByClass.js
@@ -15,6 +15,14 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * Allows a section of the page to be hidden by default and displayed on command
+ * To use set the Display attribute in CSS to 'none' and add a button that calls this function
+ * 
+ * @param {type} className class of hidden tags
+ * @param {type} obj the tag that calls this function will be hidden when items are shown
+ * @returns {undefined} none
+ */
 function showByClass(className,obj)
 {
     var theElements = document.getElementsByClassName(className);
@@ -24,4 +32,41 @@ function showByClass(className,obj)
         item.style.display='';
     }  
     obj.style.display='none';
+}
+/**
+ * Show hide a element based on its id.  The element (obj) should have text eithe Show or Hide
+ * and it will change the verb to the other (hopefully appropriate one)
+ * @param {type} idName id of the object to show/hide
+ * @param {type} btnName the jQuery selector (#<element id>) that calls us containing the word Show or Hide
+ * @returns {undefined}
+ */
+function toggleShowById(idName, btnName)
+{
+    var hash = '#';
+    var elem = jQuery(hash.concat(idName));
+    var btnTxt = jQuery(btnName).text();
+    if( elem.is(":visible") )
+    {
+        elem.hide();
+        btnTxt = btnTxt.replace("Hide", "Show");
+        jQuery(btnName).text(btnTxt);
+    }
+    else
+    {
+        elem.show();
+        btnTxt = btnTxt.replace("Show", "Hide");
+        jQuery(btnName).text(btnTxt);
+    }
+}
+
+function showById(idName)
+{
+    var elem = document.getElementById(idName);
+    elem.style.display='';
+}
+
+function hideById(idName)
+{
+    var elem = document.getElementById(idName);
+    elem.style.display='none';
 }
