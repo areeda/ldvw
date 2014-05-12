86ab5832c3c889a4fc0cdd7048357a1bec17d955
Joseph Areeda <joseph.areeda@ligo.org>
2014-05-12 01:16:37 +0000

    v 2.0.7 - some documentation and minor changes add pdf as legal image type

--- ClassLibraries/jspWebUtils/src/edu/fullerton/jspWebUtils/PageItemImage.java
+++ ClassLibraries/jspWebUtils/src/edu/fullerton/jspWebUtils/PageItemImage.java
@@ -17,34 +17,86 @@
 package edu.fullerton.jspWebUtils;
 
 /**
- *
+ * A page item that contains a displayable image.  Mime types image/* and application/pdf are supported
  * @author Joseph Areeda <joe@areeda.com>
  */
 public class PageItemImage extends PageItem
 {
-    private String url;
-    private String altText;
+    private final String url;
+    private final String altText;
     private int width=0, height=0;
+    private String mime;
     
     public PageItemImage(String url, String alt, String title)
     {
         this.url = url;
         this.altText = alt;
         this.title = title == null ? "" : title;
+        mime="image/png";
     }
 
     @Override
     public String getHtml() throws WebUtilException
     {
-        String ret = String.format("<img %1$s src=\"%2$s\" ", getAttributes(),url);
-        if (!altText.isEmpty())
-            ret += String.format(" alt=\"%1$s\" ",altText);
-        if (width > 0)
-            ret += String.format(" width=\"%1$d\" ", width);
-        if (height > 0)
-            ret += String.format(" height=\"%1$d\" ", height);
-        ret += "/>\n";
-        return ret;
+        StringBuilder ret = new StringBuilder();
+        if (mime.contains("image"))
+        {
+            ret.append(String.format("<img %1$s src=\"%2$s\" ", getAttributes(),url));
+            if (!altText.isEmpty())
+            {
+                ret.append(String.format(" alt=\"%1$s\" ", altText));
+            }
+            if (width > 0)
+            {
+                ret.append(String.format(" width=\"%1$d\" ", width));
+            }
+            if (height > 0)
+            {
+                ret.append(String.format(" height=\"%1$d\" ", height));
+            }
+            ret.append("/>\n");
+        }
+        else if (mime.contains("pdf"))
+        {
+            ret.append("<object ");
+            ret.append(getAttributes());
+            ret.append(" data=\"").append(url);
+            ret.append("\" type=\"application/pdf\"");
+            
+//            if (width > 0)
+//            {
+//                ret.append(String.format(" width=\"%1$d\" ", width));
+//            }
+//            if (height > 0)
+//            {
+//                ret.append(String.format(" height=\"%1$d\" ", height));
+//            }
+            ret.append("/>\n");
+            ret.append("<p>");
+            if (!altText.isEmpty())
+            {
+                ret.append(String.format(" Your browser does not appear to have the "
+                        + "ability to display this pdf alt=\"%1$s\" ", altText));
+            }
+            ret.append("<a href=\"");
+            ret.append(url);
+            ret.append("\">Click here </a>to download the pdf.\n</p>\n");
+            ret.append("</object>\n");
+        }
+        else
+        {
+            ret.append("<p>");
+            if (!altText.isEmpty())
+            {
+                ret.append(String.format(" alt=\"%1$s\" ", altText));
+            }
+            ret.append("<a href=\"");
+            ret.append(url);
+            ret.append("\">to the PDF!</a>\n</p>\n");
+            ret.append("</object>\n");
+        }
+        
+        return ret.toString();
     }
     
     public void setDim(int width, int height)
@@ -53,4 +105,10 @@ public class PageItemImage extends PageItem
         this.height=height;
 
     }
+
+    public void setMime(String mime)
+    {
+        this.mime = mime;
+    }
+    
 }
