4f9e4d64280c6d37d4a5b1d3c39c8f7ce6c16181
Joseph Areeda <joseph.areeda@ligo.org>
2014-06-19 03:53:36 +0000

    v 2.0.12 - finally working

--- Applications/FrameWriter/framewriter.cpp
+++ Applications/FrameWriter/framewriter.cpp
@@ -7,6 +7,7 @@
 
 #include <cstdlib>
 #include <FrameL.h>
+#include <math.h>
 
 using namespace std;
 
@@ -19,33 +20,33 @@ int main( int argc, char** argv )
 
     FrameH *frame;
 
-    FrProcData *proc;
+    FrProcData *proc, *wave;
 
-    double sampleRate;
+    
 
     long nData, i, j;
 
+    frame = FrameNew( "NDS-TEST" ); 
 
+    frame->dt = 100;    // length of frame in seconds
+    frame->detectProc->longitude = -117.885108333;
+    frame->detectProc->latitude = 33.878575;
 
-    frame = FrameNew( "demo" ); /*----------------------create a 10s long frame--*/
-
-    frame->dt = 10;
-
-
-
-    sampleRate = 16384; /*-----add a 16384Hz 32bits float proc channel---*/
-
+    double sampleRate = 16384; /*-----add a 16384Hz 32bits float proc channel---*/
+    double waveRate = 16384;
     nData = sampleRate * frame->dt;
+    long nWaveData = waveRate * frame->dt;
 
-    proc = FrProcDataNew( frame, "Channel_Name", sampleRate, nData, -32 );
+    proc = FrProcDataNew( frame, "TEST:NDS-COUNT_INFRAME", sampleRate, nData, 32 );
+    wave = FrProcDataNew( frame, "TEST:NDS-200HZ_TONE", waveRate, nData, 32 );
 
-
+    double fact = 2 * M_PI * 200 / waveRate;
 
     /*-------- open output file; compression type 9, 1000 seconds per file ----*/
 
 
 
-    oFile = FrFileONewM( "test", 9, "FrFull", 1000 );
+    oFile = FrFileONewM( "NDS-TEST", 9, "FrFull", 1000 );
 
     if ( oFile == NULL )
     {
@@ -57,44 +58,38 @@ int main( int argc, char** argv )
 
 
 
-    for ( i = 0; i < 10; i++ )
-    { /*---------------------produce 10 frames ---*/
+    frame->GTimeS = 1072569616;
 
-        frame->GTimeS = frame->GTimeS + frame->dt;
+    float v;
+    wave->data->type = FR_VECT_4R;
+    proc->data->type = FR_VECT_4R;
 
+    for ( j = 0; j < proc->data->nData; j++ )
+    { /*----- update channel content--*/
 
-
-        for ( j = 0; j < proc->data->nData; j++ )
-        { /*----- update channel content--*/
-
-            proc->data->dataF[j] = j;
-        } /*-- change this to your need---*/
-
-
-
-        if ( i < 2 ) FrameDump( frame, stdout, 2 ); /*------------- just for debug---*/
+        v=(float)sin(j*fact);
+        wave->data->dataF[j] = v;
+        proc->data->dataF[j] = j/sampleRate + frame->GTimeS;
+    } 
 
 
+    FILE *out=fopen("/home/joe/t/test1.list", "w");
 
-        if ( FrameWrite( frame, oFile ) != FR_OK )
-        {
+    if ( i < 2 ) FrameDump( frame, out, 2 ); /*------------- just for debug---*/
 
-            printf( "Write error; last error:%s\n", FrErrorGetHistory( ) );
 
-            return (0 );
-        }
 
+    if ( FrameWrite( frame, oFile ) != FR_OK )
+    {
+
+        printf( "Write error; last error:%s\n", FrErrorGetHistory( ) );
+
+        return (0 );
     }
-
-
 
     FrFileOEnd( oFile ); /*------------------------------ close the output file---*/
 
-
-
     FrameFree( frame );
-
-
 
     return (0 );
 
