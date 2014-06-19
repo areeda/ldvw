452e0a5e46b20d26af7f01bdd41e36e78a0cfe68
Joseph Areeda <joseph.areeda@ligo.org>
2014-06-19 02:41:26 +0000

    v 2.0.12 - Package that will be the glue between UI and database tables

---
+++ Applications/ldvw/src/java/commonUI/package-info.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2014 Joseph Areeda <joseph.areeda at ligo.org>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Generates UI elements (eg: PageTable) for use in multiple pages
+ * Can be viewed as the interface layer between the database tables and the Page objects
+ */
+package commonUI;
