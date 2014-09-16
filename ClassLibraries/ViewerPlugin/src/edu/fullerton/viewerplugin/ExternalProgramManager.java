d118f0acfb1ae49a409463dfe0a5c04081308d17
Joseph Areeda <joseph.areeda@ligo.org>
2014-09-16 14:16:10 +0000

    0.2.20 - more support routines added

--- ClassLibraries/ViewerPlugin/src/edu/fullerton/viewerplugin/ExternalProgramManager.java
+++ ClassLibraries/ViewerPlugin/src/edu/fullerton/viewerplugin/ExternalProgramManager.java
@@ -20,15 +20,26 @@ import com.areeda.jaDatabaseSupport.Database;
 import edu.fullerton.jspWebUtils.PageItemList;
 import edu.fullerton.jspWebUtils.PageItemString;
 import edu.fullerton.jspWebUtils.WebUtilException;
+import edu.fullerton.ldvtables.ImageCoordinateTbl;
 import edu.fullerton.ldvtables.ImageTable;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * General routines for calling command line functions
@@ -41,6 +52,8 @@ public class ExternalProgramManager
     protected StringBuilder outTxt, errTxt;     // stdout and stderr from last command
     protected ImageTable imgTbl = null;
     protected ArrayList<String> env;
+    protected Path tmpPath = null;
+    protected ArrayList<File> tmpFileDirs=null; // temporary files and directories we have created
     
     /**
      * Use our server's keytab to get a new or refresh our Kerberos TGT
@@ -89,7 +102,10 @@ public class ExternalProgramManager
     {
         return runExternalProgram(command,"");
     }
-    
+    public boolean runExternalProgram(ArrayList<String> command) throws WebUtilException
+    {
+        return runExternalProgram(command,"");
+    }
     
     public boolean runExternalProgram(String command, String sendData) throws WebUtilException
     {
@@ -108,44 +124,40 @@ public class ExternalProgramManager
                 String[] a = new String[0];
                 p = Runtime.getRuntime().exec(command, env.toArray(a));
             }
-            InputStream stderrStrm = p.getErrorStream();    // attached to stderr
-            // nb: the reason in and out are reversed is that I'm talking about the external program
-            // and java is talking about this program.
-            InputStream stdoutStrm = p.getInputStream();   // attached to stdout
+            ret = getProcessOutput(p,sendData);
             
-            if (sendData != null && !sendData.isEmpty())
-            {
-                try (OutputStream stdinStrm = p.getOutputStream()) 
-                {
-                    stdinStrm.write(sendData.getBytes());
-                    stdinStrm.flush();
-                }
-            }
-            BufferedReader outRdr = new BufferedReader(new InputStreamReader(stdoutStrm));
-            String line;
-            outTxt = new StringBuilder();
-            while ((line = outRdr.readLine()) != null)
-            {
-                outTxt.append(line).append("<br>\n");
-            }
-
-            errTxt = new StringBuilder();
-            BufferedReader errRdr = new BufferedReader(new InputStreamReader(stderrStrm));
-            while ((line = errRdr.readLine()) != null)
-            {
-                errTxt.append(line).append("<br>\n");
-            }
-
-            status = p.waitFor();
-            ret = (status == 0);
         }
         catch (IOException e)
         {
             throw new WebUtilException("Error executing command [" + command + "] - " + e.getLocalizedMessage());
         }
-        catch (InterruptedException e)
+        return ret;
+    }
+
+    public boolean runExternalProgram(ArrayList<String> command, String sendData) throws WebUtilException
+    {
+        boolean ret;
+        try
         {
-            throw new WebUtilException("Command interrupted: " + e.getLocalizedMessage());
+            ExternalProgramManager.getTGT();   // we need a Kerberos ticket for this
+
+            Process p;
+            if (env == null || env.isEmpty())
+            {
+                String[] a = new String[0];
+                p = Runtime.getRuntime().exec(command.toArray(a));
+            }
+            else
+            {
+                String[] a = new String[0];
+                p = Runtime.getRuntime().exec(command.toArray(a), env.toArray(a));
+            }
+            ret = getProcessOutput(p,sendData);
+            
+        }
+        catch (IOException e)
+        {
+            throw new WebUtilException("Error executing command [" + command + "] - " + e.getLocalizedMessage());
         }
         return ret;
     }
@@ -199,5 +211,193 @@ public class ExternalProgramManager
         {
             env.add(es);
         }
+    }
+    /**
+     * Help manage temporary files/directories
+     * If you use these calls and call removeTemps we will keep track and delete all files 
+     * and directories
+     * @param prefix
+     * @return a new empty temporary directory
+     * @throws IOException 
+     * @see #getTempFile(java.lang.String) 
+     * @see #removeTemps() 
+     */
+    public File getTempDir(String prefix) throws IOException
+    {
+        File tempDir = Files.createTempDirectory("ldvw_"+prefix).toFile();
+        addTmpFile(tempDir);
+        if (tmpPath == null)
+        {
+            tmpPath = tempDir.toPath();
+        }
+        return tempDir;
+    }
+    public File getTempFile(String prefix, String ext) throws IOException
+    {
+        File tmpFile;
+        if (tmpPath == null)
+        {
+            tmpFile = Files.createTempFile(prefix, ext).toFile();
+        }
+        else
+        {
+            tmpFile = Files.createTempFile(tmpPath, prefix, ext).toFile();
+        }
+        addTmpFile(tmpFile);
+        return tmpFile;
+    }
+    /**
+     * remove all the files and directories we created
+     */
+    public void removeTemps()
+    {
+        if (tmpFileDirs != null)
+        {
+            for (File f : tmpFileDirs)
+            {
+                rm(f);
+            }
+        }
+    }
+    private void addTmpFile(File tempDir)
+    {
+        if (tmpFileDirs == null)
+        {
+            tmpFileDirs = new ArrayList<>();
+        }
+        tmpFileDirs.add(tempDir);
+    }
+
+    /**
+     * remove a file or directory (and all files and directories in that directory)
+     * 
+     * @param f what you want rubbed out
+     */
+    private void rm(File f)
+    {
+        if (f.exists())
+        {
+            if (f.isDirectory())
+            {
+                File[] listFiles = f.listFiles();
+                for (File sf : listFiles)
+                {
+                    rm(sf);
+                }
+                f.delete();
+            }
+            else
+            {
+                f.delete();
+            }
+        }
+    }
+    public File writeTempCSV(String prefix, double[][] data) throws IOException
+    {
+        File tmpFile = getTempFile(prefix, ".csv");
+        BufferedWriter bw = new BufferedWriter(new FileWriter(tmpFile));
+        for (double[] r : data)
+        {
+            bw.append(String.format("%.6f, %.20g%n", r[0],r[1]));
+        }
+        bw.close();
+        
+        return tmpFile;
+    }
+    public int addImg2Db(File outFile, Database db, String userCn) throws WebUtilException
+    {
+        int imgId = 0;      // return code 0 means no image saved
+        try
+        {
+            ImageTable itbl = new ImageTable(db);
+            
+            // read in the output image
+            long len = outFile.length();
+            if (len > 1000)
+            {
+                FileInputStream fis = null;
+                try {
+                    byte[] imgBytes = new byte[(int) len];
+                    fis = new FileInputStream(outFile);
+                    int rlen = fis.read(imgBytes);
+                    ByteArrayInputStream bis = new ByteArrayInputStream(imgBytes);
+                    imgId = itbl.addImg(userCn, bis, "image/png");
+                }
+                catch (NoSuchAlgorithmException | IOException  ex) 
+                {
+                    throw new WebUtilException("Saving image to database", ex);
+                }
+                finally 
+                {
+                    try 
+                    {
+                        if (fis != null)
+                        {
+                            fis.close();
+                        }
+                    }
+                    catch (IOException ex) 
+                    {
+                        throw new WebUtilException("Saving image to database", ex);
+                    }
+                }
+            }
+            else
+            {
+                throw new WebUtilException("Adding image to database: image is not invalid.");
+            }
+        }
+        catch (SQLException ex)
+        {
+            throw new WebUtilException("Saving image to database", ex);
+        }
+        return imgId;
+    }
+
+    private boolean getProcessOutput(Process p, String sendData) throws WebUtilException
+    {
+        boolean ret = false;
+        try
+        {
+            InputStream stderrStrm = p.getErrorStream();    // attached to stderr
+            // nb: the reason in and out are reversed is that I'm talking about the external program
+            // and java is talking about this program.
+            InputStream stdoutStrm = p.getInputStream();   // attached to stdout
+
+            if (sendData != null && !sendData.isEmpty())
+            {
+                try (OutputStream stdinStrm = p.getOutputStream())
+                {
+                    stdinStrm.write(sendData.getBytes());
+                    stdinStrm.flush();
+                }
+            }
+            BufferedReader outRdr = new BufferedReader(new InputStreamReader(stdoutStrm));
+            String line;
+            outTxt = new StringBuilder();
+            while ((line = outRdr.readLine()) != null)
+            {
+                outTxt.append(line).append("<br>\n");
+            }
+
+            errTxt = new StringBuilder();
+            BufferedReader errRdr = new BufferedReader(new InputStreamReader(stderrStrm));
+            while ((line = errRdr.readLine()) != null)
+            {
+                errTxt.append(line).append("<br>\n");
+            }
+
+            status = p.waitFor();
+            ret = (status == 0);
+        }
+        catch (InterruptedException e)
+        {
+            throw new WebUtilException("Command interrupted: " + e.getLocalizedMessage());
+        }
+        catch (IOException ex)
+        {
+            throw new WebUtilException("Running external program:", ex);
+        }
+        return ret;
     }
 }
