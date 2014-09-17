95852720a8492162065b3ba5e7711df8c26540bd
Joseph Areeda <joseph.areeda@ligo.org>
2014-09-17 18:17:23 +0000

    search for min max of all series being plotted

--- Applications/genPlot/genPlot.py
+++ Applications/genPlot/genPlot.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Generalized plotting program for ldvw, use --help option for details"""
 __author__ = 'joseph areeda'
 __version__ = '0.0.0'
 
 import os.path
 import os
-if os.environ['HOME'] is not None:
+# make sure weh have a home value in our environment
+try:
+    home = os.environ['HOME']
+except:
     os.environ['HOME'] = '/usr/local/ldvw/.config/'
 from astropy.time import Time
 import matplotlib as mpl
@@ -16,66 +20,70 @@ import plotArgs
 import sys
 import re
 
-argList=plotArgs.getargs(sys.argv[1:])  # parse command line arguments
-if argList.test :
-    print('Dump arguments, parsing, and exit requested.')
-    print('sys.argv:')
+argList = plotArgs.getargs(sys.argv[1:])  # parse command line arguments
+if argList.test:
+    print 'Dump arguments, parsing, and exit requested.'
+    print 'sys.argv:'
     print('    %s') % sys.argv
-    print('argList:')
+    print 'argList:'
     print ('   %s') % argList
     sys.exit(3)
 
 mpl.rc('font', family='sans-serif')
 mpl.rcParams['font.size'] = '16.0'
 mpl.rcParams['font.weight'] = 'book'
+#mpl.rc("text", usetex = True)
+
+
 
 # determine image dimensions (geometry)
-width=12
-height=6
-dpi=100.0
+width = 12
+height = 6
+dpi = 100.0
 
 if argList.geometry is not None:
-    m=re.search('(\d+)x(\d+)',argList.geometry)
+    m = re.search(r'(\d+)x(\d+)', argList.geometry)
     if m is not None:
         width = int(m.group(1)) / dpi
         height = int(m.group(2)) / dpi
 
-mpl.rcParams['figure.figsize']=[width,height]
-mpl.rcParams['figure.dpi']=dpi
+mpl.rcParams['figure.figsize'] = [width, height]
+mpl.rcParams['figure.dpi'] = dpi
 
 # NB: make all mpl parameter changes before creating figure
-fig=plt.figure()
+fig = plt.figure()
 
-rect=fig.patch
+rect = fig.patch
 rect.set_facecolor('w')
 
 if argList.interactive:
     plt.interactive(True)
 
-ax=fig.add_subplot(1,1,1)
+ax = fig.add_subplot(1, 1, 1)
 
-if argList.nogrid :
+if argList.nogrid:
     ax.grid(False)
 else:
     ax.grid(True)
 
-if argList.logx :
+if argList.logx:
     ax.set_xscale('log')
 
-if argList.logy :
+if argList.logy:
     ax.set_yscale('log')
 
-for fnl in argList.infile :
-    filename=fnl[0]     #<--weird but we get a list of lists back
-    if argList.verbose > 0 :
-        print("Adding: " + filename)
-    pdata=genfromtxt(filename, delimiter=',')
-    ax.plot(pdata[:,0],pdata[:,1])
+for fnl in argList.infile:
+    filename = fnl[0]     #<--weird but we get a list of lists back
+    if argList.verbose > 0:
+        print "Adding: " + filename
+    pdata = genfromtxt(filename, delimiter=',')
+    ax.plot(pdata[:, 0], pdata[:, 1])
 
 # default range for plot is full range, but user can override any or all values
-minmax = [np.min(pdata[:,0]), np.max(pdata[:,0]), np.min(pdata[:,1]), np.max(pdata[:,1])]
+minmax = [np.min(pdata[:, 0]), np.max(pdata[:, 0]), np.min(pdata[:, 1]),
+          np.max(pdata[:, 1])]
 if argList.xmin is not None:
-    minmax[0]=float(argList.xmin)
+    minmax[0] = float(argList.xmin)
 
 if argList.xmax is not None:
     minmax[1] = float(argList.xmax)
@@ -89,47 +97,47 @@ if argList.ymax is not None:
 ax.axes.axis(minmax)
 
 # if they didn't give us a title make one up
-clist=[]
-minGps=None
-maxGps=None
-plottype=''
-title=''
+clist = []
+minGps = None
+maxGps = None
+plottype = ''
+title = ''
 if argList.title is None:
-    for fnl in argList.infile :
-        filename=fnl[0]
-        base=os.path.basename(filename)
-        f=re.search('(Spectrum_)?(.*)_(\d+)_(\d+)\.csv',base)
-        
+    for fnl in argList.infile:
+        filename = fnl[0]
+        base = os.path.basename(filename)
+        f = re.search(r'(Spectrum_)?(.*)_(\d+)_(\d+)\.csv', base)
+
         if f is not None:
-            plottype=f.group(1)[0:len(f.group(1))-1]
+            plottype = f.group(1)[0:len(f.group(1))-1]
             if f.group(2) not in clist:
                 clist.append(f.group(2))
 
-            stGps=int(f.group(3))
-            dur=int(f.group(4))
-            
+            stGps = int(f.group(3))
+            dur = int(f.group(4))
+
             if minGps is None:
-                minGps=stGps
+                minGps = stGps
             else:
                 minGps = min(stGps, minGps)
 
             if maxGps is None:
                 maxGps = stGps + dur
             else:
-                maxGps = max( maxGps, stGps + dur)
+                maxGps = max(maxGps, stGps + dur)
 
         title = plottype
         if len(clist) > 0:
             title += " " + clist[0]
-            for c in clist[1:] :
+            for c in clist[1:]:
                 title += ", " + c
 
         if minGps is not None:
-            t=Time(minGps,format='gps')
+            t = Time(minGps, format='gps')
             title += "\n%s - %10d (%ds)" % (t.iso, minGps, maxGps-minGps)
 else:
-    title=''
-    for t in argList.title :
+    title = ''
+    for t in argList.title:
         if t != '':
             title += "\n"
         title += t
@@ -158,5 +166,5 @@ if argList.interactive:
     plt.show()
 
 if argList.out is not None:
-    plt.savefig(argList.out,facecolor=fig.get_facecolor(), edgecolor='none')
+    plt.savefig(argList.out, facecolor=fig.get_facecolor(), edgecolor='none')
 
