1c3ac6dc19517b07b15bdedd835a2a2bfbade3a5
Joseph Areeda <joseph.areeda@ligo.org>
2014-12-25 01:00:40 +0000

    0.2.35 - better error handling

--- Applications/ldvw/src/java/edu/fullerton/ldvplugin/WplotManager.java
+++ Applications/ldvw/src/java/edu/fullerton/ldvplugin/WplotManager.java
@@ -22,6 +22,7 @@ import edu.fullerton.jspWebUtils.Page;
 import edu.fullerton.jspWebUtils.PageItemList;
 import edu.fullerton.jspWebUtils.PageItemString;
 import edu.fullerton.jspWebUtils.WebUtilException;
+import edu.fullerton.ldvjutils.LdvTableException;
 import edu.fullerton.ldvtables.ViewUser;
 import edu.fullerton.plugindefn.PluginController;
 import edu.fullerton.plugindefn.WplotDefinition;
@@ -31,6 +32,8 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.commons.io.FilenameUtils;
 
 /**
@@ -68,68 +71,81 @@ public class WplotManager extends ExternalPlotManager implements PlotProduct
     @Override
     public ArrayList<Integer> makePlot(ArrayList<ChanDataBuffer> dbuf, boolean compact) throws WebUtilException
     {
-        WplotDefinition wpd = new WplotDefinition();
-        wpd.init();
-        
-        // make sure wplot's sample frequency <= acquired sample frequency
-        String smplFrqName = wpd.getNamespace() + "_" + "smplfrq";
-        String[] sampleFrequency = paramMap.get(smplFrqName);
-        if (sampleFrequency != null && sampleFrequency.length == 1)
+        try
         {
-            String strFreq = sampleFrequency[0].trim();
-            if (! strFreq.matches("^\\d+$"))
-            {
-                throw new WebUtilException(String.format("Sample frequency (%1$s) is not an integer."
-                        ,strFreq));
-            }
-            Integer requested = Integer.parseInt(strFreq);
-            Float rate = dbuf.get(0).getChanInfo().getRate();
-            if(requested > rate)
-            {
-                sampleFrequency[0] = String.format("%1$.0f", rate);
-                paramMap.put(smplFrqName, sampleFrequency);
-            }
-        }
-        
-        wpd.setFormParameters(paramMap);
-        
-        String cmd = wpd.getCommandLine(dbuf,paramMap);
-        vpage.add(cmd);
-        vpage.addBlankLines(2);
-        ArrayList<Integer> ret = new ArrayList<>();
-        if (runExternalProgram(cmd))
-        {
-            String txtOutput = String.format("%1$s Output:<br>%2$s",getProductName(),getStdout());
-            vpage.add(new PageItemString(txtOutput,false));
-            vpage.addBlankLines(1);
-            txtOutput = String.format("%1$s <br>Stderr: %2$s", getProductName(), getStderr());
-            vpage.add(new PageItemString(txtOutput, false));
-            vpage.addBlankLines(1);
+            WplotDefinition wpd = new WplotDefinition();
+            wpd.init();
             
-            File outDir = wpd.getTempDir();
-            ArrayList<File> imgs = getAllImgFiles(outDir);
-            for(File file : imgs)
+            // make sure wplot's sample frequency <= acquired sample frequency
+            String smplFrqName = wpd.getNamespace() + "_" + "smplfrq";
+            String[] sampleFrequency = paramMap.get(smplFrqName);
+            if (sampleFrequency != null && sampleFrequency.length == 1)
             {
-                String desc = getDescription(file);
-                String mime = "image/" + FilenameUtils.getExtension(file.getAbsolutePath());
-                int imgId = importImage(file, mime, desc);
-                if (imgId > 0)
+                try {
+                    String strFreq = sampleFrequency[0].trim();
+                    if (! strFreq.matches("^\\d+$"))
+                    {
+                        throw new WebUtilException(String.format("Sample frequency (%1$s) is not an integer."
+                                ,strFreq));
+                    }
+                    Integer requested = Integer.parseInt(strFreq);
+                    Float rate = dbuf.get(0).getChanInfo().getRate();
+                    if(requested > rate)
+                    {
+                        sampleFrequency[0] = String.format("%1$.0f", rate);
+                        paramMap.put(smplFrqName, sampleFrequency);
+                    }
+                }
+                catch (LdvTableException ex)
                 {
-                    ret.add(imgId);
+                    throw new WebUtilException("Making omega plot", ex);
                 }
             }
+            
+            wpd.setFormParameters(paramMap);
+            
+            String cmd = wpd.getCommandLine(dbuf,paramMap);
+            vpage.add(cmd);
+            vpage.addBlankLines(2);
+            ArrayList<Integer> ret = new ArrayList<>();
+            if (runExternalProgram(cmd))
+            {
+                String txtOutput = String.format("%1$s Output:<br>%2$s",getProductName(),getStdout());
+                vpage.add(new PageItemString(txtOutput,false));
+                vpage.addBlankLines(1);
+                txtOutput = String.format("%1$s <br>Stderr: %2$s", getProductName(), getStderr());
+                vpage.add(new PageItemString(txtOutput, false));
+                vpage.addBlankLines(1);
+                
+                File outDir = wpd.getTempDir();
+                ArrayList<File> imgs = getAllImgFiles(outDir);
+                for(File file : imgs)
+                {
+                    String desc = getDescription(file);
+                    String mime = "image/" + FilenameUtils.getExtension(file.getAbsolutePath());
+                    int imgId = importImage(file, mime, desc);
+                    if (imgId > 0)
+                    {
+                        ret.add(imgId);
+                    }
+                }
+            }
+            else
+            {
+                int stat = getStatus();
+                String stderr = getStderr();
+                vpage.add(String.format("%1$s returned and error: %2$d", getProductName(),stat));
+                vpage.addBlankLines(1);
+                PageItemString ermsg = new PageItemString(stderr, false);
+                vpage.add(ermsg);
+            }
+            
+            return ret;
         }
-        else
+        catch (LdvTableException ex)
         {
-            int stat = getStatus();
-            String stderr = getStderr();
-            vpage.add(String.format("%1$s returned and error: %2$d", getProductName(),stat));
-            vpage.addBlankLines(1);
-            PageItemString ermsg = new PageItemString(stderr, false);
-            vpage.add(ermsg);
+            throw new WebUtilException("Making Omega plot", ex);
         }
-        
-        return ret;
     }
 
     @Override
