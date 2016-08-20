a4257e8b449cecd5cfc3d5fd1ddbc2fc64a2d096
Joseph Areeda <joseph.areeda@ligo.org>
2016-08-20 13:07:50 -0700

    Netbeans stuff

--- Applications/checkNdsProxy/nbproject/Makefile-variables.mk
+++ Applications/checkNdsProxy/nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=GNU-Linux-x86
-CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
 CND_ARTIFACT_NAME_Debug=checkndsproxy
-CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/checkndsproxy
-CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/checkndsproxy
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
 CND_PACKAGE_NAME_Debug=checkndsproxy.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/checkndsproxy.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/checkndsproxy.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux-x86
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
